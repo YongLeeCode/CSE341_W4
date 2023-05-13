9b2ddba11a06df0755257f9f1bd9ce92e0ded33c
Yong <yonghwan311@gmail.com>
2023-05-13 16:40:26 -0600

    access

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use((req, res, next) =>{
-  // res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   // res.setHeader(
     // 'Access-Control-Allow-Origin',
     // 'Origin, X-Requested-With, content-Type, Accept, Z-Key'
